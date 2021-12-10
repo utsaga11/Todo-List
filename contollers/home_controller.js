d8a5639ee8a6a8ea8ba689cd9e70969943eb7da0
utsaga11 <utsaga22@gmail.com>
2021-12-11 02:16:04 +0530

    creating structute

--- contollers/home_controller.js
+++ contollers/home_controller.js
@@ -1,3 +1,3 @@
 module.exports.home = function(req,res){
-    return res.render('home',{title: "komu"});
+    return res.render('home',{title: "Todo-List"});
 }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 8000;
 app.set('view engine','ejs');
 app.set('views', './views');
 
+app.use(express.static('./assets'));
+
 app.use('/', require('./routes/index'));
 
 app.listen(port, (err)=>{
