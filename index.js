a2c0f1b80a4af47ea05f726e018a3156b940d9c7
utsaga11 <utsaga22@gmail.com>
2021-12-10 13:52:01 +0530

    Initialisation 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.set('view engine','ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes/index'));
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log("Error in connecting to port");
+    }
+    console.log("Successfully connected to the port",port);
+});
